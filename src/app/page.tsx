47d8cdd850a2e5e82f2e4ba0603dba0f0e5b3b5d
Pangeran Pentoel <pangeranpentoel@gmail.com>
2025-07-15 01:28:49 +0000

    tambahkan animasi setiap mengunjungi halaman

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between animate-fade-in opacity-0">
       <div className="w-full relative">
         <Header />
         <Hero />
