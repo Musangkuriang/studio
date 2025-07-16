9933d129bf285fc785a8d407b10c4406c75d137f
Pangeran Pentoel <pangeranpentoel@gmail.com>
2025-07-16 01:57:50 +0000

    kembalikan apa yang sudah di ganti tadi

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -22,7 +22,7 @@ export default function About() {
             <div className="absolute -inset-4 bg-gradient-to-br from-purple-200 via-blue-200 to-white rounded-full opacity-30 blur-xl"></div>
              <div className="relative w-80 h-80 sm:w-96 sm:h-96">
                 <Image
-                    src="https://i.imghippo.com/files/WoL4988dco.png"
+                    src="https://i.ibb.co/sKq5x9b/e-B3s-Yt-E.png"
                     alt="Foto profil Andi Siswanto"
                     fill
                     sizes="(max-width: 640px) 320px, (max-width: 1024px) 384px, 384px"
